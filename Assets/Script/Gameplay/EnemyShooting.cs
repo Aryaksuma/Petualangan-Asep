176bb54e48b9990be0b63e64537a681d3b243309
Aryakusuma <aryakusuma1278@gmail.com>
2023-12-04 03:52:11 +0800

    pindah asset

--- Assets/Script/Gameplay/EnemyShooting.cs
+++ Assets/Script/Gameplay/EnemyShooting.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class EnemyShooting : MonoBehaviour
 {
+
+    public GameObject bullet;
+    public Transform bulletPos;
+
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,17 @@ public class EnemyShooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if(timer > 2)
+        {
+            timer = 0;
+            shoot();
+        }
+    }
+
+    void shoot()
+    {
+        Instantiate(bullet, bulletPos.position, Quaternion.identity);
     }
 }
